c9c194444870c3d0884d9a97ab771278f50ba159
aleksandrdrot <aleksandr_drot@rambler.ru>
2023-02-08 21:11:19 +0300

    Домашка по классам

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,11 @@
 public class Book {
     private int publishingYear;
     private String bookName;
+    public Author author;
 
 
-    public Book(String bookName, int publishingYear){
+    public Book(String firstName, String lastName, String bookName, int publishingYear){
+        author = new Author(firstName, lastName);
         this.bookName = bookName;
         this.publishingYear = publishingYear;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        Author tolstoy = new Author("Лев", "Толстой");
-        Author pushkin = new Author("Александр", "Пушкин");
-        Book tolstoyBook = new Book("Война и мир", 1975);
-        Book pushkinBook = new Book("Сборник стихов", 1978);
+        Book tolstoyBook = new Book("Лев","Толстой","Война и мир", 1975);
+        Book pushkinBook = new Book("Александр", "Пушкин","Сборник стихов", 1978);
 
 
-        System.out.println("Книга1 "+ tolstoy.getFirstName() + " " + tolstoy.getLastName() + " " + tolstoyBook.getBookName() + " " + tolstoyBook.getPublishingYear());
-        System.out.println("Книга2 "+ pushkin.getFirstName() + " " + pushkin.getLastName() + " " + pushkinBook.getBookName() + " " + pushkinBook.getPublishingYear());
+        System.out.println("Книга1 "+ tolstoyBook.author.getFirstName() + " " + tolstoyBook.author.getLastName() + " " + tolstoyBook.getBookName() + " " + tolstoyBook.getPublishingYear());
+        System.out.println("Книга2 "+ pushkinBook.author.getFirstName() + " " + pushkinBook.author.getLastName() + " " + pushkinBook.getBookName() + " " + pushkinBook.getPublishingYear());
         pushkinBook.setPublishingYear(1925);
-        System.out.println("Книга2 "+ pushkin.getFirstName() + " " + pushkin.getLastName() + " " + pushkinBook.getBookName() + " " + pushkinBook.getPublishingYear());
+        System.out.println("Книга2 "+ pushkinBook.author.getFirstName() + " " + pushkinBook.author.getLastName() + " " + pushkinBook.getBookName() + " " + pushkinBook.getPublishingYear());
+
     }
 
 }
